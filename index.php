8b542834de17a6f833e11a80f7bc24225544300e
sajith <usnc99@gmail.com>
2021-06-27 21:10:06 +0530

    I created my blog app

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="plugin/bootstrap.min.css">
+    <script src="plugin/jquery.min.js"></script>
+    <script src="plugin/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet"> 
+    <title>Blog App</title>
+</head>
+<body>
+    
+   <?php include_once('inc/navbar.php')?>
+
+   <div class="container">
+        <div class="row">
+           <div class="col-md-12">
+              <div class="jumbotron mt-4">
+                  <h4 id="jumbo-header">Welcomr to the DevTubes</h4>
+                  <h4 id="jumbo-emoji">👾👨‍💻👾</h4>
+              </div>
+           </div>
+        </div>
+   </div>
+   
+
+</body>
+</html>
