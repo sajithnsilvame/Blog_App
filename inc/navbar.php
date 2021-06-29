e69ce1574eb0a554220adc143123739ed29e4404
sajith <usnc99@gmail.com>
2021-06-29 16:05:04 +0530

    modified navbar.php and created new file for the Sign Up

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="index.php">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Tags</a>
@@ -31,5 +31,16 @@
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form> -->
+
+     <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Sign In</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="sign_up.php">Sign Up</a>
+        </li>
+     </ul>
+
   </div>
 </nav>
---
+++ sign_up.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="plugin/bootstrap.min.css">
+    <script src="plugin/jquery.min.js"></script>
+    <script src="plugin/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/sign_up.css">
+    <title>Blog App</title>
+</head>
+<body>
+    
+   <?php include_once('inc/navbar.php')?>
+
+   <div class="container">
+        <div class="row">
+           <div class="col-md-12">
+
+              <form action="index.php">
+                
+                    <div class="card mt-4">
+                        <div class="card-header" id="card-header">
+                            <h4>Sign Up Form</h4>
+                        </div>
+                        <div class="card-body" id="card-body">
+
+                            <div class="form-group">
+                            <label for="">First Name</label>
+                            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                            <small id="helpId" class="text-muted">Enter your first name</small>
+                            </div>
+
+                            <div class="form-group">
+                            <label for="">Last Name</label>
+                            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                            <small id="helpId" class="text-muted">Enter your last name</small>
+                            </div>
+
+                            <div class="form-group">
+                            <label for="">Email</label>
+                            <input type="email" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                            <small id="helpId" class="text-muted">Enter your email address</small>
+                            </div>
+
+
+                            <div class="form-group">
+                            <label for="">Password</label>
+                            <input type="password" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                            <small id="helpId" class="text-muted">Create your own password</small>
+                            </div>
+
+                        </div>
+                        <div class="card-footer" id="card-footer">
+                            <button type="submit" class="btn btn-primary">Sign Up</button>
+                        </div>
+
+                   </form>
+                </div>
+
+              </div>
+           </div>
+        </div>
+   </div>
+   
+
+</body>
+</html>
